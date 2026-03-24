adf76825af4d64f00ca3c434911e7169e03d9fff
ycc07 <whycc07@outlook.com>
2026-03-24 17:01:50 +0800

    Update config.ts
    Just modify a little info--website,author,profile,title

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "https://astro.121295.xyz/", // replace this with your deployed domain
+  author: "Joe Wang",
+  profile: "",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "AstroBlog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
